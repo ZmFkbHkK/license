7e948f19552d842fcad04fd44d3da96498240d1e
ZmFkbHkK <fadliwaykanan@gmail.com>
2025-04-02 12:48:26 +0700

    Update invalid.sh

--- invalid.sh
+++ invalid.sh
@@ -17,7 +17,7 @@ echo -e "   1 IP Address : 8.000 Rp"
 echo -e "   5 IP Address : 30.000 Rp"
 echo -e "   10 IP Address : 50.000 Rp"
 echo
-echo -e "  Unlimited + free access bot: 150.000 Rp"
+echo -e "  Unlimited IP + free access bot: 150.000 Rp"
 echo
 echo -e " \033[34mWhatsapp  : +6285935195701 \033[0m"
 echo -e " \033[34mTelegram  : @xdxl_raal \033[0m"
